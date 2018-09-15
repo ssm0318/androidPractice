a1bbb41cb7edf554e52f1d619b10ea26484a6895
JaeWon Kim <kleenex0318@gmail.com>
2018-09-15 17:24:16 +0900

    constraint layout

--- practice_app/app/src/main/java/android_study/practice_app/MainActivity.kt
+++ practice_app/app/src/main/java/android_study/practice_app/MainActivity.kt
@@ -1,7 +1,7 @@
 package android_study.practice_app
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
