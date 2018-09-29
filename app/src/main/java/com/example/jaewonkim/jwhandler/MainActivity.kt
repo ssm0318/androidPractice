45dd1b9b94f174c9d59f99e3b1ef6ef9e4909890
JaeWon Kim <kleenex0318@gmail.com>
2018-09-29 15:29:57 +0900

    handler

--- app/src/main/java/com/example/jaewonkim/jwhandler/MainActivity.kt
+++ app/src/main/java/com/example/jaewonkim/jwhandler/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.jaewonkim.jwhandler
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         var thread = Thread {
             run {
                 Thread.sleep(3000)
-                textView_main.setText("쓰레드 작동")
+                Handler(Looper.getMainLooper()).post {
+                    textView_main.text = "쓰레드 작동"
+                }
             }
         }
         thread.start()
