008fb9809d0a2cad6e8fc99ff817ec6529cc452c
ssm0318 <kleenex0318@gmail.com>
2018-10-04 16:53:22 +0900

    comments (almost) complete

--- todo_app/app/src/main/java/com/example/jaewonkim/todo_app/MainActivity.kt
+++ todo_app/app/src/main/java/com/example/jaewonkim/todo_app/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ItemRowListener {
+    //할 일들을 가지고 있는 array 비스무리한 것 (array 보다 자유롭게 변경 가능한 형태의 list)
     private var items: MutableList<ToDoItem> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +20,19 @@ class MainActivity : AppCompatActivity(), ItemRowListener {
         val listView = findViewById<ListView>(R.id.main_listview)
         val fab = findViewById<View>(R.id.floatingActionButton3) as FloatingActionButton
 
+        //list view 에 데이터 뿌려주는 adapter 지정.
         listView.adapter = MyCustomAdapter(this, items)
 
-        fab.setOnClickListener { view ->
+        //새로운 할 일을 추가하는 버튼을 누르면
+        fab.setOnClickListener {
+            //'새로운 할 일을 추가해보세요'라는 안내문이 사라지고
             textView.visibility = View.INVISIBLE
+            //할 일 추가를 위한 팝업이 뜬다.
             addNewItemDialog()
         }
     }
 
+    //새로운 할 일 추가
     private fun addNewItemDialog() {
         val alert = AlertDialog.Builder(this)
 
@@ -63,6 +69,7 @@ class MainActivity : AppCompatActivity(), ItemRowListener {
         alert.show()
     }
 
+    //완료 여부 상태의 변경
     override fun modifyItemState(itemObjectId: Int, isDone: Boolean) {
         val currItem = items[itemObjectId]
         val listView = findViewById<ListView>(R.id.main_listview)
@@ -74,29 +81,29 @@ class MainActivity : AppCompatActivity(), ItemRowListener {
         var t = listView.findViewWithTag<TextView>(tTag)
         var d = listView.findViewWithTag<TextView>(dTag)
 
+        //완료했다고 체크되면 제목과 설명 텍스트를 strike through
         if(items[itemObjectId].done == true) {
             t.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
             d.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
-        } else {
+        } else { //다시 누르면 strike through 없앰
             t.setPaintFlags(0)
             d.setPaintFlags(0)
         }
+        //변경 시 바로 view 에 반영되도록 변경사항이 있음을 adapter에 알려줌.
         (listView.adapter as MyCustomAdapter).notifyDataSetChanged()
     }
 
+    //할 일 내용 수정
     override fun modifyItemContent(itemObjectId: Int, itemTitle: String, itemDescription: String) {
-        val editBtn = findViewById<ImageButton>(R.id.imageButton6) as ImageButton
-        println("I'm here")
-
         addEditItemDialog(itemObjectId)
     }
 
+    //할 일 삭제
     override fun onItemDelete(itemObjectId: Int) {
-        val delBtn = findViewById<ImageButton>(R.id.imageButton5) as ImageButton
-
         confirmDelete(itemObjectId)
     }
 
+    //할 일 수정 함수 (할 일 추가와 같은 로직)
     private fun addEditItemDialog(itemObjectId: Int) {
         val alert = AlertDialog.Builder(this)
 
@@ -109,13 +116,14 @@ class MainActivity : AppCompatActivity(), ItemRowListener {
 
         val currItem = items[itemObjectId]
 
+        // 기존 내용 보여주기
         itemEditTitle.setText(currItem.itemTitle)
         itemEditDescription.setText(currItem.itemDescription)
 
         layout.addView(itemEditTitle)
         layout.addView(itemEditDescription)
-        alert.setMessage("새로운 할 일 추가")
-        alert.setTitle("할 일을 입력하세요")
+        alert.setMessage("기존 할 일 수정")
+        alert.setTitle("수정할 정보를 입력하세요")
 
         alert.setView(layout)
 
@@ -134,6 +142,7 @@ class MainActivity : AppCompatActivity(), ItemRowListener {
         alert.show()
     }
 
+    //할 일 삭제 함수
     private fun confirmDelete(itemObjectId: Int) {
         val alert = AlertDialog.Builder(this)
         val listView = findViewById<ListView>(R.id.main_listview)
@@ -143,6 +152,7 @@ class MainActivity : AppCompatActivity(), ItemRowListener {
             items.removeAt(itemObjectId)
             Toast.makeText(this, "삭제되었습니다.", Toast.LENGTH_LONG).show()
             dialog.dismiss()
+            //삭제된 할 일이 없어진 상태로 view 가 변경되도록 adapter 에 변경사항이 있음을 알려줌.
             (listView.adapter as MyCustomAdapter).notifyDataSetChanged()
         }
 
--- todo_app/app/src/main/java/com/example/jaewonkim/todo_app/MyCustomAdapter.kt
+++ todo_app/app/src/main/java/com/example/jaewonkim/todo_app/MyCustomAdapter.kt
@@ -10,9 +10,10 @@ import android.widget.ImageButton
 import android.widget.TextView
 
 class MyCustomAdapter(context: Context, private val items: MutableList<ToDoItem>): BaseAdapter() {
-    private val mContext: Context
-    private var rowListner: ItemRowListener = context as ItemRowListener
+    private val mContext: Context //이것의 필요성에 대한 설명이 필요합니당...1
+    private var rowListner: ItemRowListener = context as ItemRowListener //각 행 (할 일)에 대한 변동사항에 귀를 기울임
 
+    //이것의 필요성에 대한 설명이 필요합니당...2
     init {
         mContext = context
     }
@@ -22,46 +23,49 @@ class MyCustomAdapter(context: Context, private val items: MutableList<ToDoItem>
 
         val view: View
         val vh: ListRowHolder
+        //받아 온 view 에서 보여줄게 없을 경우
         if(convertView == null) {
             view = layoutInflater.inflate(R.layout.main_row, parent, false)
             vh = ListRowHolder(view)
-            view.tag = vh
-        } else {
+            view.tag = vh  //설명 필요...1
+        } else { //기존의 view 에서 사용할 수 있는게 있으면
             view = convertView
-            vh = view.tag as ListRowHolder
+            vh = view.tag as ListRowHolder //설명 필요...2
         }
 
+        //각 row 의 제목 뿌려줌
         val titleTextView = view.findViewById<TextView>(R.id.title_listview)
         val idx = position + 1
         titleTextView.text = "#" + idx.toString() + " " + items.get(position).itemTitle
         titleTextView.tag = "title" + position.toString()
 
+        //각 row 의 상세정보 뿌려줌
         val descriptionTextView = view.findViewById<TextView>(R.id.description_listview)
         descriptionTextView.text = items.get(position).itemDescription
         descriptionTextView.tag = "description" + position.toString()
 
         val editTextBtn = view.findViewById<ImageButton>(R.id.imageButton6)
-        editTextBtn.tag = position
+        editTextBtn.tag = position //몇 번째 index 의 할 일에 대한 버튼인지 저장.
 
         val delTextBtn = view.findViewById<ImageButton>(R.id.imageButton5)
-        delTextBtn.tag = position
+        delTextBtn.tag = position //몇 번째 index 의 할 일에 대한 버튼인지 저장.
 
         val checkbox = view.findViewById<CheckBox>(R.id.checkBox)
-        checkbox.tag = position
+        checkbox.tag = position //몇 번째 index 의 할 일에 대한 버튼인지 저장.
 
         vh.isDone.setOnClickListener {
-            val idx = vh.isDone.getTag() as Int
-            rowListner.modifyItemState(idx, !items[idx].done!!)
+            val idx = vh.isDone.getTag() as Int  //몇 번째 index 의 할 일에 대한 버튼인지 불러옴.
+            rowListner.modifyItemState(idx, !items[idx].done!!) //해당 번호의 할 일 완료 여부 상태 변경
         }
 
         vh.edit.setOnClickListener {
-            val idx = vh.edit.getTag() as Int
-            rowListner.modifyItemContent(idx, items[idx].itemTitle.toString(), items[idx].itemDescription.toString())
+            val idx = vh.edit.getTag() as Int //몇 번째 index 의 할 일에 대한 버튼인지 불러옴.
+            rowListner.modifyItemContent(idx, items[idx].itemTitle.toString(), items[idx].itemDescription.toString()) //해당 번호의 할 일 내용 변경
         }
 
         vh.del.setOnClickListener {
-            val idx = vh.del.getTag() as Int
-            rowListner.onItemDelete(idx)
+            val idx = vh.del.getTag() as Int //몇 번째 index 의 할 일에 대한 버튼인지 불러옴.
+            rowListner.onItemDelete(idx) //해당 번호의 할 일 삭제
         }
 
         return view
